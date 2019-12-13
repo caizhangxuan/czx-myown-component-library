73c588d14841f1768e5ff8bb7cd2c843be761a4d
caizhangxuan <1183268728@qq.com>
2019-12-13 11:08:03 +0800

    npm test

---
+++ src/components/MyIcon.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg class="icon" aria-hidden="true">
+      <use :xlink:href="name"></use>
+    </svg>
+</template>
+
+<script>
+  export default {
+    name: "MyIcon",
+    props: ['name','size'],
+    data() {
+      return{
+
+      }
+    },
+    mounted(){
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
